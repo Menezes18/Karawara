5bae18a0fba30180812e82602f6e9f8b1b5d5ebe
Zerfi0rd <seiyagalo@gmail.com>
2024-03-05 20:49:58 -0300

    Adicionando força quando transiciona do estado estacionario

--- Assets/InputActions/Characters/Player/PlayerDashingState.cs
+++ Assets/InputActions/Characters/Player/PlayerDashingState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,35 @@ namespace RPGKarawara
 {
     public class PlayerDashingState : PlayerGroundedState
     {
+        private PlayerDashData dashData;
+
         public PlayerDashingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine){
-            
+            dashData = movementData.DashData;
         }
+
+        #region IState Methods
+        public override void Enter()
+        {
+            base.Enter();
+
+            stateMachine.ReusableData.MovementSpeedModifier = movementData.DashData.SpeedModifier;
+
+            AddForceOnTransitionFromStationaryState();
+        }
+        #endregion
+
+        #region Main Methods
+        private void AddForceOnTransitionFromStationaryState(){
+            if(stateMachine.ReusableData.MovementInput != Vector2.zero){
+                return;
+            }
+
+            Vector3 characterRotationDirection = stateMachine.Player.transform.forward;
+
+            characterRotationDirection.y = 0f;
+
+            stateMachine.Player.Rigidbody.velocity = characterRotationDirection * GetMovementSpeed();
+        }
+        #endregion
     }
 }
