fa8d11604c51bda5327c7e86638c292c7c3c5e44
Menezes18 <gustavo.e.menezes@outlook.com>
2024-05-10 17:27:11 -0300

    Animação do persona

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SG
+{
+    public class TitleScreenManager : MonoBehaviour
+    {
+        
+
+        public void StartNewGame()
+        {
+            StartCoroutine(WorldSaveGameManager.instance.LoadNewGame());
+        }
+    }
+}
