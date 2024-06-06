487ecae7656833129ca664629c48faf2b22d90d7
Menezes18 <gustavo.e.menezes@outlook.com>
2024-06-05 22:56:15 -0300

    Sistema de resolução

--- Assets/Scripts/Menu/MenuSettings.cs
+++ Assets/Scripts/Menu/MenuSettings.cs
@@ -1,39 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+using System.IO;
+using System.Linq;
 
 namespace RPGKarawara
 {
     public class MenuSettings : MonoBehaviour
     {
-        public GameObject objectToActivate;
-        public GameObject objectToDeactivate;
+  public TMP_Dropdown dropdown;
 
-       
-        public Button toggleButton;
+        private Resolution[] resolutions;
 
         void Start()
         {
-            
-            if (toggleButton != null)
+            // Obtém as resoluções disponíveis
+            resolutions = Screen.resolutions;
+            dropdown.ClearOptions();
+
+            // Usa um HashSet para garantir que cada resolução seja única
+            HashSet<string> uniqueResolutions = new HashSet<string>();
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
             {
-                toggleButton.onClick.AddListener(ToggleObjects);
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                if (uniqueResolutions.Add(option))
+                {
+                    options.Add(option);
+                }
+
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = uniqueResolutions.Count - 1;
+                }
             }
+
+            dropdown.AddOptions(options);
+            dropdown.value = currentResolutionIndex;
+            dropdown.RefreshShownValue();
+
+            // Adiciona o listener para o evento de mudança do dropdown
+            dropdown.onValueChanged.AddListener(delegate {
+                DropdownValueChanged(dropdown);
+            });
         }
 
-        void ToggleObjects()
+        void DropdownValueChanged(TMP_Dropdown change)
         {
-            
-            if (objectToActivate != null)
-            {
-                objectToActivate.SetActive(true);
-            }
+            // Obtém o índice selecionado
+            int selectedIndex = change.value;
 
-            if (objectToDeactivate != null)
-            {
-                objectToDeactivate.SetActive(false);
-            }
+            // Define a resolução com base na seleção
+            SetResolution(selectedIndex);
+
+            // Obtém o nome da opção selecionada
+            string selectedOption = change.options[selectedIndex].text;
+
+
+        }
+
+        void SetResolution(int resolutionIndex)
+        {
+            string[] dimensions = dropdown.options[resolutionIndex].text.Split('x');
+            int width = int.Parse(dimensions[0].Trim());
+            int height = int.Parse(dimensions[1].Trim());
+
+            Screen.SetResolution(width, height, Screen.fullScreen);
         }
     }
 }
