470c4f87dfd107df16319a4d56d8c4ed514cc1d9
Menezes18 <gustavo.e.menezes@outlook.com>
2024-06-05 23:05:03 -0300

    sistema de Hz no menu

--- Assets/Scripts/Menu/MenuSettings.cs
+++ Assets/Scripts/Menu/MenuSettings.cs
@@ -9,19 +9,24 @@ namespace RPGKarawara
 {
     public class MenuSettings : MonoBehaviour
     {
-  public TMP_Dropdown dropdown;
+        public TMP_Dropdown resolutionDropdown;
+        public TMP_Dropdown refreshRateDropdown;
 
         private Resolution[] resolutions;
+        private Dictionary<string, List<int>> resolutionToRefreshRates;
 
         void Start()
         {
             // Obtém as resoluções disponíveis
             resolutions = Screen.resolutions;
-            dropdown.ClearOptions();
+            resolutionDropdown.ClearOptions();
+            refreshRateDropdown.ClearOptions();
+            
 
             // Usa um HashSet para garantir que cada resolução seja única
             HashSet<string> uniqueResolutions = new HashSet<string>();
-            List<string> options = new List<string>();
+            resolutionToRefreshRates = new Dictionary<string, List<int>>();
+            List<string> resolutionOptions = new List<string>();
 
             int currentResolutionIndex = 0;
             for (int i = 0; i < resolutions.Length; i++)
@@ -29,47 +34,74 @@ namespace RPGKarawara
                 string option = resolutions[i].width + " x " + resolutions[i].height;
                 if (uniqueResolutions.Add(option))
                 {
-                    options.Add(option);
+                    resolutionOptions.Add(option);
+                    resolutionToRefreshRates[option] = new List<int>();
                 }
+
+                resolutionToRefreshRates[option].Add(resolutions[i].refreshRate);
 
                 if (resolutions[i].width == Screen.currentResolution.width &&
                     resolutions[i].height == Screen.currentResolution.height)
                 {
-                    currentResolutionIndex = uniqueResolutions.Count - 1;
+                    currentResolutionIndex = resolutionOptions.IndexOf(option);
                 }
             }
 
-            dropdown.AddOptions(options);
-            dropdown.value = currentResolutionIndex;
-            dropdown.RefreshShownValue();
+            resolutionDropdown.AddOptions(resolutionOptions);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
 
-            // Adiciona o listener para o evento de mudança do dropdown
-            dropdown.onValueChanged.AddListener(delegate {
-                DropdownValueChanged(dropdown);
+            // Preenche o dropdown de taxas de atualização com base na resolução atual
+            UpdateRefreshRateDropdown(resolutionOptions[currentResolutionIndex]);
+
+            // Adiciona o listener para o evento de mudança do dropdown de resolução
+            resolutionDropdown.onValueChanged.AddListener(delegate {
+                ResolutionDropdownValueChanged(resolutionDropdown);
+            });
+
+            // Adiciona o listener para o evento de mudança do dropdown de taxa de atualização
+            refreshRateDropdown.onValueChanged.AddListener(delegate {
+                RefreshRateDropdownValueChanged(refreshRateDropdown);
             });
         }
 
-        void DropdownValueChanged(TMP_Dropdown change)
+        void ResolutionDropdownValueChanged(TMP_Dropdown change)
         {
-            // Obtém o índice selecionado
-            int selectedIndex = change.value;
+            // Obtém a resolução selecionada
+            string selectedResolution = change.options[change.value].text;
 
-            // Define a resolução com base na seleção
-            SetResolution(selectedIndex);
+            // Atualiza o dropdown de taxas de atualização com base na resolução selecionada
+            UpdateRefreshRateDropdown(selectedResolution);
 
-            // Obtém o nome da opção selecionada
-            string selectedOption = change.options[selectedIndex].text;
-
-
+            // Define a resolução com a taxa de atualização padrão (primeira da lista)
+            SetResolution(change.value, 0);
         }
 
-        void SetResolution(int resolutionIndex)
+        void RefreshRateDropdownValueChanged(TMP_Dropdown change)
         {
-            string[] dimensions = dropdown.options[resolutionIndex].text.Split('x');
+            // Define a resolução com base na resolução e taxa de atualização selecionadas
+            SetResolution(resolutionDropdown.value, change.value);
+        }
+
+        void UpdateRefreshRateDropdown(string resolution)
+        {
+            refreshRateDropdown.ClearOptions();
+
+            List<int> refreshRates = resolutionToRefreshRates[resolution];
+            List<string> refreshRateOptions = refreshRates.Select(rate => rate + " Hz").Distinct().ToList();
+            refreshRateDropdown.AddOptions(refreshRateOptions);
+            refreshRateDropdown.value = 0;
+            refreshRateDropdown.RefreshShownValue();
+        }
+
+        void SetResolution(int resolutionIndex, int refreshRateIndex)
+        {
+            string[] dimensions = resolutionDropdown.options[resolutionIndex].text.Split('x');
             int width = int.Parse(dimensions[0].Trim());
             int height = int.Parse(dimensions[1].Trim());
+            int refreshRate = resolutionToRefreshRates[resolutionDropdown.options[resolutionIndex].text][refreshRateIndex];
 
-            Screen.SetResolution(width, height, Screen.fullScreen);
+            Screen.SetResolution(width, height, Screen.fullScreen, refreshRate);
         }
     }
 }
