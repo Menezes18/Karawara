73c0608df8f9cb242b21cc6d36919505776b58c6
Arcmigs <miguel-felipe2010@hotmail.com>
2024-06-06 22:59:58 -0300

    Melhora no tiro

--- Assets/Vídeo/MagicSpeel.cs
+++ Assets/Vídeo/MagicSpeel.cs
@@ -9,9 +9,11 @@ namespace RPGKarawara
         public static MagicSpeel instance;
         public GameObject tiro;
         public Transform inicio;
-        public Transform player; // Referência para o jogador
+        public Transform player; // Referï¿½ncia para o jogador
         public float speed = 10f; // Velocidade do tiro
         public float lifetime = 5f; // Tempo de vida do tiro
+        
+        [field: SerializeField]public float pursueTime = 0.5f;
 
         public void Start()
         {
@@ -21,29 +23,42 @@ namespace RPGKarawara
         public void Tiro(Transform Player)
         {
             player = Player;
-            // Instancia o objeto 'tiro' na posição e rotação definidas por 'inicio'
+            // Instancia o objeto 'tiro' na posiï¿½ï¿½o e rotaï¿½ï¿½o definidas por 'inicio'
             GameObject tiroInstance = Instantiate(tiro, inicio.position, inicio.rotation);
 
-            // Inicia a coroutine para mover o tiro em direção ao jogador
+            // Inicia a coroutine para mover o tiro em direï¿½ï¿½o ao jogador
             StartCoroutine(MoveTowardsPlayer(tiroInstance));
         }
 
         IEnumerator MoveTowardsPlayer(GameObject tiroInstance)
         {
             float elapsedTime = 0;
+            Vector3 direction = Vector3.zero; // Armazena a direï¿½ï¿½o do tiro
+
+            while (elapsedTime < pursueTime)
+            {
+                // Move o tiro em direï¿½ï¿½o ao jogador
+                direction = (new Vector3(player.position.x, player.position.y + 1.4f, player.position.z) - tiroInstance.transform.position).normalized;
+                tiroInstance.transform.position += direction * speed * Time.deltaTime;
+
+                elapsedTime += Time.deltaTime;
+                
+                yield return null;
+            }
 
             while (elapsedTime < lifetime)
             {
-                // Move o tiro em direção ao jogador
-                tiroInstance.transform.position = Vector3.MoveTowards(tiroInstance.transform.position,new Vector3(player.position.x, player.position.y + 1.4f,player.position.z), speed * Time.deltaTime);
+                // Move o tiro na ï¿½ltima direï¿½ï¿½o que estava seguindo
+                tiroInstance.transform.position += direction * speed * Time.deltaTime;
 
                 elapsedTime += Time.deltaTime;
 
                 yield return null;
             }
 
-            // Destroi o tiro após 5 segundos
+            // Destroi o tiro apï¿½s 5 segundos
             Destroy(tiroInstance);
         }
+
     }
 }
