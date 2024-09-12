d68768c7965669a8c5117868bedc920a57835347
Arcmigs <miguel-felipe2010@hotmail.com>
2024-09-12 03:14:17 -0300

    Finalizando o tutorial inicial

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -9,96 +9,115 @@ namespace RPGKarawara
     public class DialogueSystem : MonoBehaviour
     {
         public TextMeshProUGUI dialogueText; // Arraste e solte o componente de texto da UI aqui no Inspector
-        public string[] dialogueLines; 
+        public string[] dialogueLines;
         public int currentLineIndex = -1;
+        public int cenas = 0;
+        bool wait = false;
 
         void Update()
         {
-            if ((Keyboard.current.wKey.wasPressedThisFrame && currentLineIndex == -1) || 
-            (Keyboard.current.aKey.wasPressedThisFrame && currentLineIndex == -1) || 
-            (Keyboard.current.sKey.wasPressedThisFrame && currentLineIndex == -1) || 
-            (Keyboard.current.dKey.wasPressedThisFrame && currentLineIndex == -1))
+            if (cenas == 0)
             {
-                ShowNextDialogueLine();
-                return;
-            }
+                if ((Keyboard.current.wKey.wasPressedThisFrame && currentLineIndex == -1) ||
+                (Keyboard.current.aKey.wasPressedThisFrame && currentLineIndex == -1) ||
+                (Keyboard.current.sKey.wasPressedThisFrame && currentLineIndex == -1) ||
+                (Keyboard.current.dKey.wasPressedThisFrame && currentLineIndex == -1))
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if ((Keyboard.current.wKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
-            (Keyboard.current.aKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
-            (Keyboard.current.sKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
-            (Keyboard.current.dKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0 )
-            )
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if ((Keyboard.current.wKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) ||
+                (Keyboard.current.aKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) ||
+                (Keyboard.current.sKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) ||
+                (Keyboard.current.dKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0)
+                )
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if ((Keyboard.current.wKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) || 
-            (Keyboard.current.aKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) ||
-            (Keyboard.current.sKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) || 
-            (Keyboard.current.dKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) )
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if ((Keyboard.current.wKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) ||
+                (Keyboard.current.aKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) ||
+                (Keyboard.current.sKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) ||
+                (Keyboard.current.dKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1))
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 2)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if (Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 2)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 3)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 3)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 4)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 4)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 5)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 5)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if ((Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) || 
-            (Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) || 
-            (Keyboard.current.digit3Key.wasPressedThisFrame && currentLineIndex == 6))
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if ((Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) ||
+                (Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) ||
+                (Keyboard.current.digit3Key.wasPressedThisFrame && currentLineIndex == 6))
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 7)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
+                if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 7)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
 
-            if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 8)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
-            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 9)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
-            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 10)
-            {
-                ShowNextDialogueLine();
-                return;
-            }
-            if (Keyboard.current.anyKey.wasPressedThisFrame && currentLineIndex == 11)
-            {
-                ShowNextDialogueLine();
-                return;
+                if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 8)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
+                if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 9)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
+                if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 10)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
+                if (Keyboard.current.anyKey.wasPressedThisFrame && currentLineIndex == 11)
+                {
+                    ShowNextDialogueLine();
+                    return;
+                }
+            } else {
+                if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == -1 && wait == false)
+                {
+                    wait = true;
+                    Invoke("ShowNextDialogueLine", 4f);
+                    return;
+                }
+
+                if (Mouse.current.middleButton.wasPressedThisFrame && currentLineIndex == 0 && wait == false)
+                {
+                    wait = true;
+                    Invoke("ShowNextDialogueLine", 4f);
+                    return;
+                }
             }
         }
 
@@ -106,19 +125,23 @@ namespace RPGKarawara
         {
             if (dialogueLines.Length > 0)
             {
-                
+
                 currentLineIndex++;
-                if (currentLineIndex >= dialogueLines.Length){
-                    dialogueText.text =  "";
+                if (currentLineIndex >= dialogueLines.Length)
+                {
+                    dialogueText.text = "";
                 }
-                else{
+                else
+                {
                     dialogueText.text = dialogueLines[currentLineIndex];
-                    
+
                 }
             }
-            else{
+            else
+            {
                 Debug.Log("AAA");
             }
+            wait = false;
         }
     }
 }
