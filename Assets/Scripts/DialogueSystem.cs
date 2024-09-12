aa01734bd2266f12eac7c4dee15cc2a4a05ff3aa
Arcmigs <miguel-felipe2010@hotmail.com>
2024-09-12 02:57:10 -0300

    Tutorial melhorado

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -10,13 +10,95 @@ namespace RPGKarawara
     {
         public TextMeshProUGUI dialogueText; // Arraste e solte o componente de texto da UI aqui no Inspector
         public string[] dialogueLines; 
-        private int currentLineIndex = 0;
+        public int currentLineIndex = -1;
 
         void Update()
         {
-            if (Keyboard.current.enterKey.wasReleasedThisFrame)
+            if ((Keyboard.current.wKey.wasPressedThisFrame && currentLineIndex == -1) || 
+            (Keyboard.current.aKey.wasPressedThisFrame && currentLineIndex == -1) || 
+            (Keyboard.current.sKey.wasPressedThisFrame && currentLineIndex == -1) || 
+            (Keyboard.current.dKey.wasPressedThisFrame && currentLineIndex == -1))
             {
                 ShowNextDialogueLine();
+                return;
+            }
+
+            if ((Keyboard.current.wKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
+            (Keyboard.current.aKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
+            (Keyboard.current.sKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0) || 
+            (Keyboard.current.dKey.isPressed && Keyboard.current.shiftKey.wasPressedThisFrame && currentLineIndex == 0 )
+            )
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if ((Keyboard.current.wKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) || 
+            (Keyboard.current.aKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) ||
+            (Keyboard.current.sKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) || 
+            (Keyboard.current.dKey.isPressed && Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 1) )
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Keyboard.current.ctrlKey.wasPressedThisFrame && currentLineIndex == 2)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 3)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 4)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Keyboard.current.tabKey.wasPressedThisFrame && currentLineIndex == 5)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if ((Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) || 
+            (Keyboard.current.digit1Key.wasPressedThisFrame && currentLineIndex == 6) || 
+            (Keyboard.current.digit3Key.wasPressedThisFrame && currentLineIndex == 6))
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 7)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+
+            if (Keyboard.current.escapeKey.wasPressedThisFrame && currentLineIndex == 8)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 9)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+            if (Mouse.current.leftButton.wasPressedThisFrame && currentLineIndex == 10)
+            {
+                ShowNextDialogueLine();
+                return;
+            }
+            if (Keyboard.current.anyKey.wasPressedThisFrame && currentLineIndex == 11)
+            {
+                ShowNextDialogueLine();
+                return;
             }
         }
 
