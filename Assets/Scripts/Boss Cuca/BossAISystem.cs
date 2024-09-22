b598d2957cd5bc150e54270f7851f82541377e64
Menezes18 <gustavo.e.menezes@outlook.com>
2024-09-22 18:56:10 -0300

    dash funcional e attack melee do boss

--- Assets/Scripts/Boss Cuca/BossAISystem.cs
+++ Assets/Scripts/Boss Cuca/BossAISystem.cs
@@ -33,6 +33,12 @@ namespace RPGKarawara
         [SerializeField]
         private DamageBoss _damageBossCollider;
 
+        [SerializeField] private float dashTimer = 2.5f;
+        public bool AttackMelee = false;
+        public RaycastHit hit;
+        private Vector3 directionToPlayer;
+        [SerializeField]
+        private bool canDash = false;
         void Start()
         {
             animator = GetComponent<Animator>();
@@ -45,12 +51,19 @@ namespace RPGKarawara
             // Faz o boss olhar para o jogador
             if (player != null)
             {
-                Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-                if (directionToPlayer != Vector3.zero) // Previne divisão por zero
-                {
-                    Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // Ajuste a suavidade
+                directionToPlayer = (player.transform.position - transform.position).normalized;
+                if (Physics.Raycast(transform.position, directionToPlayer, out hit, Mathf.Infinity)){
+                    if (hit.collider.CompareTag("Player") && !isDashing &&
+                        hasLaughed){
+                        canDash = true;
+                    }
                 }
+                // Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+                // if (directionToPlayer != Vector3.zero) // Previne divisão por zero
+                // {
+                //     Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
+                //     transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // Ajuste a suavidade
+                // }
             }
 
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadiusPlayer);
@@ -74,9 +87,9 @@ namespace RPGKarawara
             // Espera até o fim da risada
             yield return new WaitForSeconds(Risada.length);
             FireProjectile();
-            PlayShootAnimation();
+            //PlayShootAnimation();
             
-            yield return new WaitForSeconds(1f); // Pequeno atraso antes de iniciar o dash
+            yield return new WaitForSeconds(2.5f); // Pequeno atraso antes de iniciar o dash
             DashTowardsPlayer();
         }
 
@@ -100,64 +113,95 @@ namespace RPGKarawara
         void PlayShootAnimation()
         {
             // Chamar a animação de disparo
-            animator.SetTrigger("Shoot");
+            //animator.SetTrigger("Shoot");
         }
 
         void DashTowardsPlayer()
         {
             if (player != null && !isDashing)
             {
-                // Realiza um raycast para garantir que o jogador está na linha de visão
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, (player.transform.position - transform.position).normalized, out hit))
-                {
-                    if (hit.collider.CompareTag("Player"))
+               
+                    if (canDash)
                     {
                         StartCoroutine(DashCoroutine(hit.point));
                     }
-                }
+                
             }
         }
 
         private bool hasDamagedPlayer = false; // Adicione esta variável
 
-        IEnumerator DashCoroutine(Vector3 targetPosition)
+    IEnumerator DashCoroutine(Vector3 targetPosition)
+    {
+        Debug.Log("AA");
+        _damageBossCollider.AtivarCollider();
+        isDashing = true;
+
+        // Manter a posição y da Cuca constante (ficando no chão)
+        float fixedY = transform.position.y;
+        Vector3 targetPositionFlat = new Vector3(targetPosition.x, fixedY, targetPosition.z); // Alvo no mesmo nível
+
+        // Calcular a posição alvo com um offset
+        Vector3 offset = (targetPositionFlat - transform.position).normalized * 4f; // Ajuste o valor para a distância que deseja passar
+        Vector3 finalTargetPosition = targetPositionFlat + offset;
+
+        while (Vector3.Distance(transform.position, finalTargetPosition) > 0.1f) // Tolerância de 0.1 unidades
         {
-            _damageBossCollider.AtivarCollider();
-            isDashing = true;
+            // Move o boss na direção do alvo final
+            Vector3 direction = (finalTargetPosition - transform.position).normalized;
+            transform.position += direction * (dashSpeed * Time.deltaTime);
 
-            // Manter a posição y da Cuca constante (ficando no chão)
-            float fixedY = transform.position.y;
-            Vector3 targetPositionFlat = new Vector3(targetPosition.x, fixedY, targetPosition.z); // Alvo no mesmo nível
-
-            // Calcular a posição alvo com um offset
-            Vector3 offset = (targetPositionFlat - transform.position).normalized * 4f; // Ajuste o valor para a distância que deseja passar
-            Vector3 finalTargetPosition = targetPositionFlat + offset;
-
-            while (Vector3.Distance(transform.position, finalTargetPosition) > 0.1f) // Tolerância de 0.1 unidades
+            // Aplique dano apenas uma vez durante o dash
+            if (_damageBossCollider.canDamage && !hasDamagedPlayer)
             {
-                // Move o boss na direção do alvo final
-                Vector3 direction = (finalTargetPosition - transform.position).normalized;
-                transform.position += direction * (dashSpeed * Time.deltaTime);
-
-                // Aplique dano apenas uma vez durante o dash
-                if (_damageBossCollider.canDamage && !hasDamagedPlayer)
-                {
-                    ApplyDamageToPlayer();
-                    hasDamagedPlayer = true; // Marca que o dano já foi aplicado
-                }
-
-                yield return null;
+                ApplyDamageToPlayer();
+                hasDamagedPlayer = true; // Marca que o dano já foi aplicado
             }
-
-            // Garantir que o boss pare exatamente na posição alvo
-            transform.position = finalTargetPosition;
-
-            // Resetar a variável para permitir dano em futuros dashes
-            hasDamagedPlayer = false;
-
-            isDashing = false;
+            yield return null;
         }
+
+        // Garantir que o boss pare exatamente na posição alvo
+        transform.position = finalTargetPosition;
+
+        // Resetar a variável para permitir dano em futuros dashes
+        hasDamagedPlayer = false;
+
+        isDashing = false;
+
+        
+        
+        CallMethodBasedOnChance();
+        _damageBossCollider.DesativarCollider();
+    }
+
+    IEnumerator TimeAttack(){
+        yield return new WaitForSeconds(1000f);
+        AttackMelee = true;
+    }
+    void CallMethodBasedOnChance(){
+        StartCoroutine(TimeAttack());
+        float chance = Random.value; // Gera um número entre 0.0 e 1.0
+        if (chance <= 0.3f) // 30% de chance
+        {
+            MethodA();
+        }
+        else // 70% de chance
+        {
+            MethodB();
+        }
+    }
+
+    void MethodA()
+    {
+        Debug.Log("Método A chamado!"); // Método que será chamado 30% das vezes
+        // Adicione aqui a lógica que você deseja para o Método A
+    }
+
+    void MethodB()
+    {
+        Debug.Log("Método B chamado!"); // Método que será chamado 70% das vezes
+        // Adicione aqui a lógica que você deseja para o Método B
+    }
 
 
 
---
+++ Assets/Scripts/Boss Cuca/MeleeBoss/Scripts/PlayerControl.cs
@@ -0,0 +1,228 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Random = UnityEngine.Random;
+using RPGKarawara;
+public class EnemyAI : MonoBehaviour
+{
+    [Space]
+    [Header("Components")]
+    [SerializeField] private Animator anim;
+    [SerializeField] private Transform player; // Referência ao jogador
+
+    [Space]
+    [Header("Combat")]
+    [SerializeField] private Transform attackPos;
+    [SerializeField] private float quickAttackDeltaDistance;
+    [SerializeField] private float heavyAttackDeltaDistance;
+    [SerializeField] private float knockbackForce = 10f;
+    [SerializeField] private float airknockbackForce = 10f;
+    [SerializeField] private float attackRange = 1f;
+    [SerializeField] private float reachTime = 0.3f;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private string PlayerTag = "PlayerLock";
+
+    private bool isAttacking = false;
+    private Coroutine attackCoroutine;
+
+    [Space]
+    [Header("Detection")]
+    [SerializeField] private float detectionRange = 5f;
+
+    [SerializeField] private BossAISystem _bossAISystem;
+    private void Start(){
+        player = GameObject.FindGameObjectWithTag(PlayerTag).transform;
+        _bossAISystem = FindObjectOfType<BossAISystem>();
+    }
+
+    void Update()
+    {
+        HandleAI();
+        HandleAttackToggle();
+    }
+
+    void HandleAI()
+    {
+        if (player == null) return;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange && !isAttacking && _bossAISystem.AttackMelee)
+        {
+            FaceThis(player.position);
+            StartAttacking(); // Inicia o loop de ataque
+        }
+        // else if (distanceToPlayer > detectionRange && isAttacking)
+        // {
+        //     StopAttacking(); // Para o ataque se o jogador sair da detecção
+        // }
+    }
+
+    void HandleAttackToggle()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     if (isAttacking)
+        //     {
+        //         StopAttacking();
+        //     }
+        //     else
+        //     {
+        //         StartAttacking();
+        //     }
+        // }
+    }
+
+    void StartAttacking()
+    {
+        if (attackCoroutine == null)
+        {
+            attackCoroutine = StartCoroutine(AttackLoop());
+        }
+    }
+
+    void StopAttacking()
+    {
+        if (attackCoroutine != null)
+        {
+            StopCoroutine(attackCoroutine);
+            attackCoroutine = null;
+            ResetAttack();
+        }
+    }
+
+    private IEnumerator AttackLoop()
+    {
+        isAttacking = true;
+
+        while (true)
+        {
+            Attack(Random.Range(0, 2)); // Ataca de forma aleatória (0 ou 1)
+            yield return new WaitForSeconds(1f); // Espera um segundo entre os ataques
+        }
+    }
+
+    public void Attack(int attackState)
+    {
+        if (isAttacking)
+        {
+            RandomAttackAnim(attackState);
+            PerformAttack(); // Chame PerformAttack aqui para aplicar o dano
+        }
+    }
+
+    private void RandomAttackAnim(int attackState)
+    {
+        switch (attackState)
+        {
+            case 0: // Quick Attack
+                QuickAttack();
+                break;
+            case 1: // Heavy Attack
+                HeavyAttack();
+                break;
+        }
+    }
+
+    void QuickAttack()
+    {
+        int attackIndex = Random.Range(1, 4);
+        MoveTowardsTarget(player.position, quickAttackDeltaDistance, "punch"); // Mova antes da animação
+        switch (attackIndex)
+        {
+            case 1: // punch
+                anim.SetBool("punch", true);
+                break;
+            case 2: // kick
+                anim.SetBool("kick", true);
+                break;
+            case 3: // mmakick
+                anim.SetBool("mmakick", true);
+                break;
+        }
+    }
+
+    void HeavyAttack()
+    {
+        int attackIndex = Random.Range(1, 3);
+        FaceThis(player.position);
+        switch (attackIndex)
+        {
+            case 1: // heavyAttack1
+                anim.SetBool("heavyAttack1", true);
+                break;
+            case 2: // heavyAttack2
+                anim.SetBool("heavyAttack2", true);
+                break;
+        }
+    }
+
+    public void ResetAttack()
+    {
+        anim.SetBool("punch", false);
+        anim.SetBool("kick", false);
+        anim.SetBool("mmakick", false);
+        anim.SetBool("heavyAttack1", false);
+        anim.SetBool("heavyAttack2", false);
+        // Não redefine isAttacking aqui
+    }
+
+    public void PerformAttack()
+    {
+        Collider[] hitPlayers = Physics.OverlapSphere(attackPos.position, attackRange);
+        foreach (Collider player in hitPlayers){
+            if (player.CompareTag(PlayerTag)) {// Verifica se o objeto tem a tag "Player"
+                Rigidbody playerRb = player.GetComponent<Rigidbody>();
+                if (playerRb != null){
+                    // Vector3 knockbackDirection = player.transform.position - transform.position;
+                    //knockbackDirection.y = airknockbackForce;
+                    // playerRb.AddForce(knockbackDirection.normalized * knockbackForce, ForceMode.Impulse);
+                    // Adicione aqui a lógica para causar dano ao jogador
+                }
+        }
+    }
+    }
+
+    public void MoveTowardsTarget(Vector3 target_, float deltaDistance, string animationName_)
+    {
+        PerformAttackAnimation(animationName_);
+        Vector3 finalPos = TargetOffset(target_, deltaDistance);
+        finalPos.y = transform.position.y; // Mantém o eixo Y atual
+        transform.DOMove(finalPos, reachTime);
+    }
+
+
+    void PerformAttackAnimation(string animationName_)
+    {
+        anim.SetBool(animationName_, true);
+    }
+
+    public Vector3 TargetOffset(Vector3 target, float deltaDistance)
+    {
+        return Vector3.MoveTowards(target, transform.position, deltaDistance);
+    }
+
+    public void FaceThis(Vector3 target)
+    {
+        Quaternion lookAtRotation = Quaternion.LookRotation(target - transform.position);
+        lookAtRotation.x = 0;
+        lookAtRotation.z = 0;
+        transform.rotation = lookAtRotation;
+    }
+    public void GetClose()
+    {
+        Vector3 targetPosition = player.position;
+        targetPosition.y = transform.position.y; // Mantém o eixo Y atual
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, quickAttackDeltaDistance);
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange); // Visualiza a área de detecção
+    }
+}
