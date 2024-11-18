57179180f215a892341189bafcaf8c8461d54361
Menezes18 <gustavo.e.menezes@outlook.com>
2024-11-18 05:30:06 -0300

    Ultimas coisas para prebanca, reviver

---
+++ Assets/TriggerFallCheck.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPGKarawara
+{
+    public class TriggerFallCheck : MonoBehaviour
+    {
+        public float minY = 19f;
+        public SavePositionOnTrigger savePositionScript;
+
+        void Start()
+        {
+            // Verifica se a cena ativa é "Video1"
+            if (SceneManager.GetActiveScene().name == "Video1")
+            {
+                // Obtém a referência do script SavePositionOnTrigger
+                savePositionScript = FindObjectOfType<SavePositionOnTrigger>();
+            }
+        }
+
+        void Update()
+        {
+            // Verifica se a cena ativa é "Video1"
+            if (SceneManager.GetActiveScene().name != "Video1")
+                return;
+
+            Debug.Log(transform.position.y);
+
+            // Checa se a posição Y do player está abaixo da altura mínima
+            if (transform.position.y <= minY)
+            {
+                // Chama o método Reviver() se o player cair
+                if (savePositionScript != null)
+                {
+                    savePositionScript.Reviver();
+                }
+                else
+                {
+                    Debug.LogWarning("Script SavePositionOnTrigger não encontrado!");
+                }
+            }
+        }
+    }
+}
