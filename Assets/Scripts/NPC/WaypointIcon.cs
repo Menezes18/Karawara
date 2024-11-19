9806508d27318a34cb1247900cd361927b03df31
Menezes18 <gustavo.e.menezes@outlook.com>
2024-11-19 09:21:57 -0300

    Melhora no tutorial, agora é collider trigger

--- Assets/Scripts/NPC/WaypointIcon.cs
+++ Assets/Scripts/NPC/WaypointIcon.cs
@@ -9,28 +9,31 @@ using UnityEngine;
         public GameObject waypointIcon;
         public string waypointName;
         public bool showWaypointIcon;
+        
+        public bool isActive;
         public void Start(){
             waypointIcon =  GameObject.Find(waypointName);
         }
-        
         private void FixedUpdate(){
             if (waypointName == null){
                 waypointIcon =  GameObject.Find(waypointName);
             }
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !isActive)
+            {
+                isActive = true;
+                TutorialManager.instance.ChamarCurrentTutorial();
+            }
+        }
 
         public void AtivarIcon(){
-            
-            
             waypointIcon.SetActive(true);
         }
         public void DesativarIcon(){
-            
-            
-                waypointIcon.SetActive(false);
-                
-            
+            waypointIcon.SetActive(false);
         }
         
     }
