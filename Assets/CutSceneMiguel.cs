cf3d4f49b2de8bd44066ab0f8a02269017d6f9db
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-12-06 20:32:05 -0300

    CutSceneMiguel

---
+++ Assets/CutSceneMiguel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RPGKarawara
+{
+    public class CutSceneMiguel : MonoBehaviour
+    {
+        public GameObject CameraPlayer, CameraNPC;
+        public Transform player; 
+        public float interactionRange = 3f;
+        public AudioSource audio;
+       private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            //CameraPlayer = GameObject.FindGameObjectWithTag("PlayerCamera (Left & Right)");
+            //dialogBox.SetActive(false); // Esconde a caixa de diálogo no início
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Vector3.Distance(transform.position, player.position) <= interactionRange)
+            {
+                // Verifica se a tecla E foi pressionada
+                if (Keyboard.current.eKey.wasPressedThisFrame)
+                {
+                    CameraPlayer.SetActive(false);
+                    CameraNPC.SetActive(true);
+                    GetComponent<Animator>().SetBool("Cutscene",true);
+                    audio.Play();
+                }   
+            }
+        }
+    }
+}
