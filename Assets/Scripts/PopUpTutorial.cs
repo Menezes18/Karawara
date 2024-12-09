27778a37f44b5eaab79174ce91a792408e0b87a7
Arcmigs <miguel-felipe2010@hotmail.com>
2024-12-09 16:44:22 -0300

    melhora vídeo tutorial

--- Assets/Scripts/PopUpTutorial.cs
+++ Assets/Scripts/PopUpTutorial.cs
@@ -4,6 +4,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace RPGKarawara
 {
@@ -11,28 +12,46 @@ namespace RPGKarawara
     {
         public GameObject UI;
         public tutorialInfo info;
-        public GameObject player;
+        public VideoPlayer videoPlayer;
+        public bool passou = false;
 
-        void Start()
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
+        void Update(){
+            if(passou){
+                Destroy(gameObject);
+            }
+
+            if(UI.activeSelf == true){
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                if(Time.timeScale > 0){
+                    Time.timeScale = 0;
+                }
+            }
         }
+
         public void OnTriggerEnter(Collider other){
             gameObject.GetComponent<BoxCollider>().enabled = false;
             Time.timeScale = 0;
+            Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
             UI.GetComponentInChildren<TMP_Text>().text = info.textoExplicativo;
-            UI.GetComponentInChildren<RawImage>().texture = info.video;
+            UI.GetComponentInChildren<RawImage>().texture = info.texture;
+            UI.GetComponentInChildren<Button>().onClick.AddListener(close);
+            videoPlayer.clip = info.vClip;
+            videoPlayer.targetTexture = info.texture;
+            videoPlayer.Play();
             UI.SetActive(true);
         }
 
         public void close(){
-            player.SetActive(true);
             UI.SetActive(false);
             Time.timeScale = 1;
             Cursor.lockState = CursorLockMode.Locked;
             UI.GetComponentInChildren<TMP_Text>().text = "";
             UI.GetComponentInChildren<RawImage>().texture = null;
+            videoPlayer.clip = null;
+            videoPlayer.targetTexture = null;
+            passou = true;
         }
 
     }
