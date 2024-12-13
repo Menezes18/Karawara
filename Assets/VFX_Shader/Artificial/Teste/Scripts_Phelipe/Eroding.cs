cf856dc1e46b49591ec27d0c3158b9745bc66c80
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-12-13 16:56:14 -0300

    Ultimas mudancas

--- Assets/VFX_Shader/Artificial/Teste/Scripts_Phelipe/Eroding.cs
+++ Assets/VFX_Shader/Artificial/Teste/Scripts_Phelipe/Eroding.cs
@@ -8,36 +8,60 @@ public class Eroding : MonoBehaviour
     public float erodeRate = 0.03f;
     public float erodeRefreshRate = 0.01f;
     public float erodeDelay = 1.25f;
-    public SkinnedMeshRenderer erodeObject;
+    public SkinnedMeshRenderer[] erodeObjects;
+    public Animator _animTainara;
+    public GameObject Lanca;
+
+    void Start()
+    {
+        Lanca = GameObject.FindGameObjectWithTag("Lanca");
+    }
     void Update()
     {
         if (Keyboard.current.tabKey.wasPressedThisFrame)
         {
-            StartCoroutine(ErodeObject());
-            Debug.Log(erodeObject.material.GetFloat("_Erode"));
+            
+            foreach (SkinnedMeshRenderer erodeObject in erodeObjects)
+            {
+                if (!erodeObject.gameObject.activeInHierarchy)
+                {
+                    erodeObject.gameObject.SetActive(true);
+                }
+                StartCoroutine(ErodeObject(erodeObject));
+                Debug.Log(erodeObject.material.GetFloat("_Erode"));
+            }
         }
     }
-    IEnumerator ErodeObject()
+
+    IEnumerator ErodeObject(SkinnedMeshRenderer erodeObject)
     {
         yield return new WaitForSeconds(erodeDelay);
-        if(erodeObject.material.GetFloat("_Erode") >= 1f){
-            float t = 1;
+        _animTainara.enabled = true;
+        if (erodeObject.material.GetFloat("_Erode") >= 1f)
+        {
+            float t = 1f;
             while (t > 0)
             {
                 t -= erodeRate;
-                for(int i = 0; i < erodeObject.materials.Length; i++)
-                erodeObject.materials[i].SetFloat("_Erode", t);
+                for (int i = 0; i < erodeObject.materials.Length; i++)
+                {
+                    erodeObject.materials[i].SetFloat("_Erode", t);
+                    Lanca.GetComponent<MeshRenderer>().material.SetFloat("_Erode", t);
+                }
                 yield return new WaitForSeconds(erodeRefreshRate);
             }
         }
-        else 
+        else
         {
-            float t = 0;
-            while (t < 1)
+            float t = 0f;
+            while (t < 1f)
             {
                 t += erodeRate;
-                for(int i = 0; i < erodeObject.materials.Length; i++)
-                erodeObject.materials[i].SetFloat("_Erode", t);
+                for (int i = 0; i < erodeObject.materials.Length; i++)
+                {
+                    erodeObject.materials[i].SetFloat("_Erode", t);
+                    //Lanca.GetComponent<MeshRenderer>().material.SetFloat("_Erode", t);
+                }
                 yield return new WaitForSeconds(erodeRefreshRate);
             }
         }
